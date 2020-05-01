0a1720ee9dc607e6df3daf8bc1dc03c2beadb040
Sergey <53207069+blog-razrabotchika@users.noreply.github.com>
2020-05-01 10:54:21 +0300

    init

---
+++ IAPProduct.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+enum IAPProduct: String {
+    case appAdd = "IAP indentifer"
+    //Add your IAP indentifers heare
+    
+    var description: String {
+        return self.rawValue
+    }
+}
